af81fb846e87edee3c9247cf54229a3055248880
92summer <80613363+92summer@users.noreply.github.com>
2023-03-12 13:45:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
+# -*- coding: utf-8 -*-
+
 #python弹幕姬,更改roomid（房间号）即可监测直播间弹幕及其他信息
 import asyncio
 import zlib
 from aiowebsocket.converses import AioWebSocket
 import json
+import os
 
 remote = 'ws://broadcastlv.chat.bilibili.com:2244/sub'
-roomid = '4716116' #4716116
+roomid = input("请输入房间号:")
+if(len(roomid)==0):
+    roomid = '4716116'
+# roomid = '4716116'
 
 data_raw = '000000{headerLen}0010000100000007000000017b22726f6f6d6964223a{roomid}7d'
-data_raw = data_raw.format(headerLen=hex(27 + len(roomid))[2:],
-roomid=''.join(map(lambda x: hex(ord(x))[2:], list(roomid))))
+data_raw = data_raw.format(headerLen=hex(27 + len(roomid))[2:], roomid=''.join(map(lambda x: hex(ord(x))[2:], list(roomid))))
 
 async def startup():
     async with AioWebSocket(remote) as aws:
         converse = aws.manipulator
         await converse.send(bytes.fromhex(data_raw))
+        # print("发送",bytes.fromhex(data_raw))
         tasks = [receDM(converse), sendHeartBeat(converse)]
         await asyncio.wait(tasks)
-hb='00 00 00 10 00 10 00 01  00 00 00 02 00 00 00 01'
+hb='00 00 00 10 00 10 00 01 00 00 00 02 00 00 00 01'
 async def sendHeartBeat(websocket):
     while True:
         await asyncio.sleep(30)
         await websocket.send(bytes.fromhex(hb))
+        # print(bytes.fromhex(hb))
         print('[Notice] Sent HeartBeat.')
 async def receDM(websocket):
     while True:
         recv_text = await websocket.receive()
-
         if recv_text == None:
             recv_text = b'\x00\x00\x00\x1a\x00\x10\x00\x01\x00\x00\x00\x08\x00\x00\x00\x01{"code":0}'
-
+        # print(recv_text)
         printDM(recv_text)
 
 
@@ -67,18 +73,21 @@ def printDM(data):
             jd = json.loads(data[16:].decode('utf-8', errors='ignore'))
             if (jd['cmd'] == 'DANMU_MSG'):
                 print('[弹幕] ', jd['info'][2][1], ': ', jd['info'][1])
-                # with open("message.json", "w", encoding='utf-8') as file:
-                #     s = data[16:].decode('utf-8', errors='ignore')
-                #     file.write(json.dumps(s, ensure_ascii=False , indent=4 , separators=(',', ':')))
+                msg = '[id]' + jd['info'][2][1] + ': ' + jd['info'][1]
+                os.system(f'mosquitto_pub -h 192.168.1.103 -p 1883 -t \'msg/danmu\' -m "{msg}"')
             elif (jd['cmd'] == 'SEND_GIFT'):
-                print('[礼物]', jd['data']['uname'], ' ', jd['data']['action'], ' ', jd['data']['num'], 'x',
-                    jd['data']['giftName'])
+                print('[礼物]', jd['data']['uname'], ' ', jd['data']['action'], ' ', jd['data']['num'], 'x', jd['data']['giftName'])
+                msg = str(jd['data']['uname'])+ ' ' + str(jd['data']['action'])+ ' ' + str(jd['data']['num'])+ ' x ' + str(jd['data']['giftName'])
+                os.system(f'mosquitto_pub -h 192.168.1.103 -p 1883 -t \'msg/danmu\' -m "{msg}"')
             elif (jd['cmd'] == 'LIVE'):
                 print('[Notice] LIVE Start!')
             elif (jd['cmd'] == 'PREPARING'):
                 print('[Notice] LIVE Ended!')
             elif (jd['cmd'] == 'INTERACT_WORD'): #谁进入直播间
                 print(jd['data']['uname'], " 进入直播间")
+                msg ="[new]" + jd['data']['uname']+ " 进入直播间"
+                os.system(f'mosquitto_pub -h 192.168.1.103 -p 1883 -t \'msg/danmu\' -m "{msg}"')
+
                 # with open("message.json", "w", encoding='utf-8') as file:
                 #     s = data[16:].decode('utf-8', errors='ignore')
                 #     file.write(json.dumps(s, ensure_ascii=False , indent=4 , separators=(',', ':')))
